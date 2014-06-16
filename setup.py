640e1468da2479d4cca8d5a55ad87dbf169aea6f
Jeff Arthur <wuftymerguftyguff@gmail.com>
2014-06-16 22:31:02 +0100

    metadata
    update metadata to add github url

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
 setup(name='uheprng',
       version='1.3',
-      description='Ultra High Entropy Pseudo Random Number Generator',
+      description='Pure Python Ultra High Entropy Pseudo Random Number Generator',
       author='Jeff Arthur',
       author_email='wuftymerguftyguff@gmail.com',
+      url='http://github.com/wuftymerguftyguff/uheprng',
       py_modules=['uheprng'],
       )
